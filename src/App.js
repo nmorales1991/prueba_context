0960423f4fbee8a8dbf0fa5bb10db42f15f1ec3c
Nicolás Morales <nicomorales297@gmail.com>
2019-03-15 10:13:04 -0300

    Subo proyecto

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ThemeContext, themes } from "./theme-context";
+import ThemedButton from "./themed-button";
 
-class App extends Component {
-  render() {
+// Un componente intermedio que utiliza ThemedButton.
+function Toolbar(props) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <ThemedButton onClick={props.changeTheme}>Change Theme</ThemedButton>
     );
-  }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            theme: themes.light
+        };
+
+        this.toggleTheme = () => {
+            this.setState(state => ({
+                theme: state.theme === themes.dark ? themes.light : themes.dark
+            }));
+        };
+    }
+
+    render() {
+        // El botón ThemedButton dentro de ThemeProvider
+        // usa el tema del estado mientras que el exterior usa
+        // el tema oscuro predeterminado
+        return (
+           <React.Fragment>
+                <ThemeContext.Provider value={this.state.theme}>
+                    <Toolbar changeTheme={this.toggleTheme} />
+                </ThemeContext.Provider>
+              
+                    <ThemedButton />
+              
+                    </React.Fragment>
+        );
+    }
 }
 
 export default App;
